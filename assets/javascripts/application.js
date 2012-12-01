39a104359ddc45fcc234b7beb3a8d4d6343872d4
blake <blake.rego@gmail.com>
2012-12-01 12:34:57 -0500

    adding jquery-ui libraries to website. adding bounce effect on page load.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,11 +1,15 @@
 $('.header_link').hover(
-		function(e)
+		function()
 		{
 			/// On-Hover, scale up the link.
 			$(this).transition({ scale: '1.2'});
 		}, 
 		function()
 		{
+			/// On-hover-leave, the link should be normal size. 
 			$(this).transition({ scale: '1'});
 		}
 	);
+
+	
+$('.container').effect("bounce", { times: 1, distance:10}, "slow");
